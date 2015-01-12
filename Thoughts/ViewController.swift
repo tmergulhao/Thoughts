2183d51980cf8fb3d3eee40658afd7b1aaf71251
Tiago Mergulhão <me@tmergulhao.com>
2015-01-12 14:10:32 -0200

    Initial Commit

---
+++ Thoughts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thoughts
+//
+//  Created by Tiago Mergulhão on 12/01/15.
+//  Copyright (c) 2015 Tiago Mergulhão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
